c519ceb651247d42552a428cbd7783ef2730ebfc
miao <miao@163.com>
2021-07-22 19:30:07 +0800

    添加路由规则

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,32 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import money from '@/views/money.vue';
+import label from '@/views/label.vue';
+import statistics from '@/views/statistics.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/money'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/#money',
+    component: money
+  },
+  {
+    path: '/#label',
+    component: label
+  },
+  {
+    path: '/#statistics',
+    component: statistics
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
