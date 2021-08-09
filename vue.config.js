84f2d735365173f1dc69adc14e790e2278772e02
miao <miao@163.com>
2021-08-09 19:14:30 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ const path = require('path')
 module.exports = {
   lintOnSave: false,
   chainWebpack: config =>{
-    const dir = path.resolve(__dirname, 'src/assets/icons')
+    const dir = path.resolve(__dirname, 'src/assets/icons') // 查找的路径
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
     config.module
       .rule('svg-sprite')
-      .test(/\.svg$/)
+      .test(/\.svg$/) // 匹配svg文件
       .include.add(dir).end() // 包含 icons 目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
       .use('svgo-loader').loader('svgo-loader')
