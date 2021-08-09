0e4dff967564c7063d3f164d9a065ea07ace7afd
miao <miao@163.com>
2021-08-09 19:15:25 +0800

    使用Vuex完成全局tag相关函数

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import {State, Tag} from '@/custom';
 Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
+const state: State = {
+  payOrIncome: "pay",
+  isAdd: "no",
+  tagList: [],
+  recordList: []
+}
+const store =  new Vuex.Store({
+  state,
   mutations: {
+    fetchTags(state): void{
+      state.tagList = JSON.parse(window.localStorage.getItem("tagList") || "")
+    },
+    saveTag(state,payload: Tag): void{
+      state.tagList.push(payload)
+      window.localStorage.setItem("tagList",JSON.stringify(state.tagList))
+      store.commit("fetchTags")
+    },
+    updateTag(state,payload: Tag): void{
+      const index = state.tagList.findIndex(item => item.id === payload.id)
+      if (index !== -1){
+        state.tagList.splice(index,1,payload)
+        window.localStorage.setItem("tagList",JSON.stringify(state.tagList))
+      }
+    },
+    removeTag(state, payload: string){
+      if (payload){
+        window.localStorage.setItem("tagList",JSON.stringify(state.tagList.filter(item => item.id !== payload)))
+        store.commit("fetchTags")
+      }
+    }
   },
   actions: {
   },
   modules: {
   }
 })
+
+export default store
