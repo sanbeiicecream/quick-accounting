92e6e951cea1f986e0d2133505630b4157ca135e
miao <miao@163.com>
2021-08-09 19:13:00 +0800

    id生成器

---
+++ src/lib/createId.ts
@@ -0,0 +1,5 @@
+let maxId: number = parseInt(window.localStorage.getItem("_maxId") || "0")
+export function createId(){
+  window.localStorage.setItem('_maxId', (++maxId).toString());
+  return maxId
+}
