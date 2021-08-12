b07ec3f7f409c4f6bb4c31c64e320370e6e9e2ee
miao <miao@163.com>
2021-08-12 21:44:43 +0800

    添加统计页面更新

---
+++ src/components/Statistics/UpdateStatistics.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>修改页</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class UpdateStatistics extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
