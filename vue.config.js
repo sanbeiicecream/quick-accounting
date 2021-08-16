59baee8ff4bd46882e0d20df31d0c4cbb3944719
miao <miao@163.com>
2021-08-16 12:31:37 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -15,18 +15,3 @@ module.exports = {
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
   }
 }
-// const path = require('path');
-//
-// module.exports = {
-//   lintOnSave: false,
-//   chainWebpack: config => {
-//     const dir = path.resolve(__dirname, 'src/assets/icons')
-//     config.module
-//       .rule('svg-sprite')
-//       .test(/\.svg$/)
-//       .include.add(dir).end()
-//       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-//     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
-//     config.module.rule('svg').exclude.add(dir)
-//   }
-// };
